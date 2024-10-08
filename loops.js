4755bf1614e318feb59385da96c03583f0489e4b
BoMaassen <bomaassen@live.nl>
2024-10-08 17:54:42 +0200

    opdracht af

--- loops.js
+++ loops.js
@@ -11,8 +11,9 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
-
-
+for (let i = 0; i < 6; i++) {
+    console.log("*".repeat(i))
+}
 
 
 // ==========================================
@@ -25,8 +26,11 @@
 // klaar!
 // ==========================================
 
-
-
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log("loop...")
+    } else console.log("Klaar!")
+}
 
 
 // ==========================================
@@ -45,8 +49,13 @@
 // >> 9
 // ==========================================
 
-
-
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i)
+    } else if (i < 6) {
+        console.log(">" + i)
+    } else console.log(">>" + i)
+}
 
 
 // ==========================================
@@ -87,5 +96,17 @@
 // etc.
 // ==========================================
 
+for (let i = 1 ; i < 101; i++){
+    if (i % 5 === 0 && i % 3 === 0){
+        console.log("FizzBuzz")
+    }
+    else if (i % 5 === 0){
+        console.log("Buzz")
+    }
+    else if (i % 3 === 0){
+        console.log("Fizz")
+    }
+    else console.log(i)
+}
 
 
